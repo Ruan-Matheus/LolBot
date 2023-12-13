9dcf2c338d736d37d03991118145cbe180337c01
Ruan Matheus <150976203+Ruan-Matheus@users.noreply.github.com>
2023-12-13 19:44:48 -0300

    Update Riot API.py
    n_solo was modificated to handles errors when the entries list is equal to one, and the pprints were commented

--- Riot API.py
+++ Riot API.py
@@ -1,17 +1,19 @@
 import requests
+import discord
+from discord import app_commands
 from pprint import pprint
 
 "Ver o elo da rapaziada com um comando com um bot de discord"
 
-key = 'RGAPI-acd6e3fa-c899-455e-80aa-27d22b0d5db3'
+api_key = 'RGAPI-acd6e3fa-c899-455e-80aa-27d22b0d5db3'
 
-players_names = ["Ruansitos", "Dutdudu", "Ferballen"]
+players_names = ["Ruansitos", "Dutdudu", "Ferballen", "n digitarei", "Mânoel Gomes"]
 player_status = {}
 players_status = []
 
 for player_name in players_names:
     try:
-        summomer_request = requests.get(f'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{player_name}?api_key={key}')
+        summomer_request = requests.get(f'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{player_name}?api_key={api_key}')
         if not summomer_request.status_code // 100 == 2:
             print(f"Error: Unexpected response {summomer_request}")
         
@@ -21,7 +23,7 @@ for player_name in players_names:
         summoner_account_id = summoner_json['accountId']
         summmoner_puuid = summoner_json['puuid']
 
-        pprint(summoner_json, indent= 4)
+#         pprint(summoner_json, indent= 4)
         
     except requests.exceptions.RequestException as e:
         # A serious problem happened, like an SSLError or InvalidURL
@@ -29,14 +31,14 @@ for player_name in players_names:
          
     try:
         # Request url for the entries of a summoner --> https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner_account_id}?api_key={api_key}
-        entries_request = requests.get(f'https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner_id}?api_key={key}')
+        entries_request = requests.get(f'https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/{summoner_id}?api_key={api_key}')
         if not entries_request.status_code // 100 == 2:
             print(f"Error: Unexpected response {entries_request}")
 
         entries_json = entries_request.json()
-        pprint(entries_json, indent= 4)
+#         pprint(entries_json, indent= 4)
             
-        n_solo = 1 if entries_json and entries_json[1]['queueType'] == 'RANKED_SOLO_5x5' else 0
+        n_solo = 1 if len(entries_json) > 1 and entries_json[1]['queueType'] == 'RANKED_SOLO_5x5' else 0
         
         player_status = {
             'Name': summoner_name,
